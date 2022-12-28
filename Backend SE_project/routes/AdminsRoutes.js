ace35cad53e3c99bb08aea4f380b999ad721c70f
PMPhuc <pmphuc20@clc.fitus.edu.vn>
2022-12-28 15:37:52 +0700

    Phúc 1st commit

--- Backend SE_project/routes/AdminsRoutes.js
+++ Backend SE_project/routes/AdminsRoutes.js
@@ -4,7 +4,13 @@ const AdminControll = require('../controllers/AdminsController');
 const router = express.Router();
 
 //-----------------------ADMINS--------------------------------------------
+router.get('/getadmins', AdminControll.get_all_admins);
+router.get('/getadmin/:id', AdminControll.get_admin_byID);
+router.get('/searchadmins/:id', AdminControll.get_admin_byLikeName);
+router.post('/addadmin', AdminControll.create_Admin);
+router.patch('/updateadmin/:id', AdminControll.update_Admin);
+router.delete('/deleteadmin/:id', AdminControll.delete_Admin);
 
 module.exports = {
     routes: router
-}
+};
