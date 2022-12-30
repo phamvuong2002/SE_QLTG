633622d58b5a3130770d8dcf229ad477bca9bb68
phamvuong2002 <117916286+phamvuong2002@users.noreply.github.com>
2022-12-30 10:43:50 +0700

    backend update author overview
    API countstory for overview page

---
+++ Backend SE_project/data/authors/countStory.sql
@@ -0,0 +1,1 @@
+EXEC [dbo].[countStory] @authorid
--- Backend SE_project/routes/AuthorsRoutes.js
+++ Backend SE_project/routes/AuthorsRoutes.js
@@ -5,12 +5,13 @@ const AuthorsControll = require('../controllers/AuthorsController');
 const router = express.Router();
 
 //-----------------------AUTHORS--------------------------------------------
-router.get('/getauthors', AuthorsControll.get_all_authors);
-router.get('/getauthor/:id', AuthorsControll.get_author_byID);
-router.get('/searchauthors/:id', AuthorsControll.get_author_byLikeName);
-router.post('/addauthor', AuthorsControll.create_Author);
-router.put('/updateauthor/:id', AuthorsControll.update_Author);
-router.delete('/deleteauthor/:id', AuthorsControll.delete_Author);
+// router.get('/getauthors', AuthorsControll.get_all_authors);
+router.get('/countstory/:id', AuthorsControll.countStory);
+router.get('/storydatalist/:id', AuthorsControll.storyDatalist);
+// router.get('/searchauthors/:id', AuthorsControll.get_author_byLikeName);
+// router.post('/addauthor', AuthorsControll.create_Author);
+// router.put('/updateauthor/:id', AuthorsControll.update_Author);
+// router.delete('/deleteauthor/:id', AuthorsControll.delete_Author);
 
 module.exports = {
     routes: router
