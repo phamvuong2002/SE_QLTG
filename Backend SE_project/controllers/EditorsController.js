6004c096bdf3cd7bb92846278bc94f743e268fe4
blackjack209 <75198184+blackjack209@users.noreply.github.com>
2022-12-31 10:20:54 +0700

    editor_commit

--- Backend SE_project/controllers/EditorsController.js
+++ Backend SE_project/controllers/EditorsController.js
@@ -3,6 +3,25 @@
 const EditorsData = require('../data/editors');
 
 
-module.exports = {
+const countStory = async (req, res, next) => {
+    try {
+        const editorid = req.params.id;
+        const num = await EditorsData.countStory(editorid);
+        res.send(num);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
 
+const storyDatalist = async (req, res, next) => {
+    try {
+        const editorid = req.params.id;
+        const list = await EditorsData.storyDatalist(editorid);
+        res.send(list);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+module.exports = {
+    countStory, storyDatalist
 }
---
+++ EDITOR_DB.sql
@@ -0,0 +1,21 @@
+create 
+--alter 
+proc editorcountStory
+	@editorid char(10)
+as
+begin
+	if not exists (select EDITORID from EDITOR where EDITORID = @editorid)
+	begin
+		SELECT 'EDITOR NOT EXISTS' AS ERROR
+		return 0
+	end
+	declare @story int = 0, @chap int = 0
+	select @story = ISNULL(COUNT(STORYID),0), @chap = ISNULL(sum(NUMOFCHAPS),0) from STORY where EDITORID = @editorid	
+	declare @earn float = 0, @unpair float = 0
+	select @earn = @earn + sum(GIA), @unpair = @unpair + sum(UNPAIR)  from CHAPTER where EDITORID = @editorid
+	select @earn = @earn + sum(GIA), @unpair = @unpair + sum(UNPAIR)  from DRAFT where EDITORID = @editorid
+	select @earn = @earn + sum(GIA), @unpair = @unpair + sum(UNPAIR)  from OUTLINE where EDITORID = @editorid
+	select @story AS numstory, @chap AS numchap, @earn AS earn, (@earn - @unpair) as 'receive'
+end
+GO
+exec editorcountStory 'ED841430  '
