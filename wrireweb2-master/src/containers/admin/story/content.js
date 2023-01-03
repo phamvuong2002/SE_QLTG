803782c00a374b9592e023d73a0e322e3f95c500
PMPhuc <pmphuc20@clc.fitus.edu.vn>
2023-01-03 16:24:43 +0700

    11th

--- wrireweb2-master/src/containers/admin/story/content.js
+++ wrireweb2-master/src/containers/admin/story/content.js
@@ -9,12 +9,11 @@ const Content = ({detail_content}) => {
     console.log(event.target.value);
   };
   return (
-    <textarea  className='w-[600px] h-[500px] bg-white shadow-md rounded-md ml-[200px]'
+    <text  className='w-[600px] h-[500px] bg-white shadow-md rounded-md ml-[200px]'
     id="message"
     name="message"
-    value={message}
     onChange={handleMessageChange}
-    ></textarea >
+    >{message}</text >
   )
 }
 
